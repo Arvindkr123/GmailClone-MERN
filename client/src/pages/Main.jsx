63d8d7675774cf28eac58c2944d318aab794e450
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-05 10:50:05 +0530

    started  added new clone

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header, Sidebar } from "../components";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <div>Display here emails</div>
+    </div>
+  );
+};
+
+export default Main;
